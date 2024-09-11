82750fb1d87e0557a9c01fc4b04059727ebd45d7
ianwang777 <b01501025@gmail.com>
2024-09-11 23:43:17 +0800

    Added search_es.py to data-generator service, which sends search query request to elasticsearch cluster repeatedly.

---
+++ search_es.py
@@ -0,0 +1,28 @@
+import time
+import requests
+
+# Elasticsearch 的 URL
+ES_URL = "http://elasticsearch1:9200/health_info/_search"
+
+# 定義查詢函數，查詢 health_info 資料
+def search_es():
+    query = {
+        "query": {
+            "match_all": {}
+        }
+    }
+    
+    try:
+        response = requests.get(ES_URL, json=query)
+        if response.status_code == 200:
+            data = response.json()
+            print("Search Result:", data)
+        else:
+            print("Search failed:", response.status_code, response.text)
+    except Exception as e:
+        print("Error during search:", e)
+
+# 每秒執行一次
+while True:
+    search_es()
+    time.sleep(1)
